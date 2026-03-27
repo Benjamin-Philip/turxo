347e3cf83993efd08e863d024176af7c65c9099c
Benjamin Philip <benjamin.philip495@gmail.com>
2026-03-27 22:14:06 +0530

    Reimplement NIF Wrapping (#4)
    This commit re-implements wrapping with macros, replacing the less
    ergonomic apply based wrap.

--- turxo/lib/turxo/nif.ex
+++ turxo/lib/turxo/nif.ex
@@ -1,23 +1,37 @@
 defmodule Turxo.NIF do
   use Rustler, otp_app: :turxo, crate: :turxo_nif
 
-  @timeout 5_000
+  db = [db_open: [:path], db_connect: [:db]]
+  conn = [conn_execute: [:conn, :sql, :params], conn_query: [:conn, :sql, :params]]
 
-  def db_open(_path), do: nif_error()
-  def db_connect(_db), do: nif_error()
-  def conn_execute(_conn, _sql, _params), do: nif_error()
-  def conn_query(_conn, _sql, _params), do: nif_error()
+  nifs = db ++ conn
+
+  for {name, args} <- nifs do
+    to_splice = Enum.map(args, fn arg -> Macro.var(arg, __MODULE__) end)
+    def unquote(name)(unquote_splicing(to_splice)), do: nif_error()
+  end
+
   defp nif_error, do: :erlang.nif_error(:nif_not_loaded)
 
-  def wrap(fun, args) do
-    ref = apply(__MODULE__, fun, args)
+  defmodule Wrapped do
+    @timeout 5_000
 
-    receive do
-      {^ref, value} ->
-        value
-    after
-      @timeout ->
-        {:error, "response not received"}
+    for {name, args} <- nifs do
+      to_splice = Enum.map(args, fn arg -> Macro.var(arg, __MODULE__) end)
+
+      def unquote(name)(unquote_splicing(to_splice)) do
+        Turxo.NIF.unquote(name)(unquote_splicing(to_splice)) |> recieve_value
+      end
+    end
+
+    defp recieve_value(ref) do
+      receive do
+        {^ref, value} ->
+          value
+      after
+        @timeout ->
+          {:error, "response not received"}
+      end
     end
   end
 end
--- turxo/test/turxo/nif_test.exs
+++ turxo/test/turxo/nif_test.exs
@@ -1,11 +1,22 @@
 defmodule Turxo.NIFTest do
   use ExUnit.Case, async: true
 
-  alias Turxo.NIF
+  alias Turxo.NIF, as: Unwrapped
+  alias Turxo.NIF.Wrapped, as: NIF
+
+  test "Wrapped correctly wraps" do
+    # Wrapped version of the unwrapped db_open test below.
+    #
+    # If the final result of both functions have the same properites,
+    # then wrapping is correct.
+
+    assert {:ok, db} = NIF.db_open(":memory:")
+    assert is_reference(db)
+  end
 
   describe "db_open/1" do
     test "can build in memory" do
-      ref = NIF.db_open(":memory:")
+      ref = Unwrapped.db_open(":memory:")
       assert is_reference(ref)
 
       assert_receive({^ref, {:ok, db}})
@@ -15,100 +26,86 @@ defmodule Turxo.NIFTest do
     @tag :tmp_dir
     test "can build a database file", %{tmp_dir: dir} do
       path = "#{dir}/test.db"
-      ref = NIF.db_open(path)
-      assert is_reference(ref)
 
-      assert_receive({^ref, {:ok, db}})
+      assert {:ok, db} = NIF.db_open(path)
       assert is_reference(db)
       assert File.exists?(path)
     end
 
     test "can safely handle invalid paths" do
-      ref = NIF.db_open("/invalid/path/")
-      assert is_reference(ref)
-
-      assert_receive({^ref, {:error, "I/O error (open): entity not found"}})
+      assert {:error, "I/O error (open): entity not found"} = NIF.db_open("/invalid/path/")
     end
   end
 
   test "db_connect/1" do
-    {:ok, db} = NIF.wrap(:db_open, [":memory:"])
-    ref = NIF.db_connect(db)
+    {:ok, db} = NIF.db_open(":memory:")
 
-    assert_receive({^ref, {:ok, conn}})
+    assert {:ok, conn} = NIF.db_connect(db)
     assert is_reference(conn)
   end
 
   describe "conn_execute/3 correctly handles" do
     setup do
-      {:ok, db} = NIF.wrap(:db_open, [":memory:"])
-      {:ok, conn} = NIF.wrap(:db_connect, [db])
+      {:ok, db} = NIF.db_open(":memory:")
+      {:ok, conn} = NIF.db_connect(db)
 
       {:ok, 0} =
-        NIF.wrap(:conn_execute, [
+        NIF.conn_execute(
           conn,
           "CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)",
           []
-        ])
+        )
 
       %{db: db, conn: conn}
     end
 
     test "no parameters", %{conn: conn} do
-      ref =
-        NIF.conn_execute(
-          conn,
-          "CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, email TEXT)",
-          []
-        )
-
-      assert_receive({^ref, {:ok, 0}})
+      assert {:ok, 0} =
+               NIF.conn_execute(
+                 conn,
+                 "CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, email TEXT)",
+                 []
+               )
     end
 
     test "positional parameters", %{conn: conn} do
-      ref =
-        NIF.conn_execute(
-          conn,
-          "INSERT INTO users (name, email) VALUES (?1, ?2)",
-          ["Alice", "alice@example.com"]
-        )
-
-      assert_receive({^ref, {:ok, 1}})
+      assert {:ok, 1} =
+               NIF.conn_execute(
+                 conn,
+                 "INSERT INTO users (name, email) VALUES (?1, ?2)",
+                 ["Alice", "alice@example.com"]
+               )
     end
 
     test "named parameters", %{conn: conn} do
-      ref =
-        NIF.conn_execute(
-          conn,
-          "INSERT INTO users (name, email) VALUES (:name, :email)",
-          name: "Alice",
-          email: "alice@example.com"
-        )
+      assert {:ok, 1} =
+               NIF.conn_execute(
+                 conn,
+                 "INSERT INTO users (name, email) VALUES (:name, :email)",
+                 name: "Alice",
+                 email: "alice@example.com"
+               )
 
-      assert_receive({^ref, {:ok, 1}})
-
-      ref =
-        NIF.conn_execute(
-          conn,
-          "INSERT INTO users (name, email) VALUES (:name, :email)",
-          name: "Alice"
-        )
-
-      assert_receive({^ref, {:ok, 1}})
+      assert {:ok, 1} =
+               NIF.conn_execute(
+                 conn,
+                 "INSERT INTO users (name, email) VALUES (:name, :email)",
+                 name: "Alice"
+               )
     end
   end
 
   describe "conn_query/3 correctly handles" do
     setup do
-      {:ok, db} = NIF.wrap(:db_open, [":memory:"])
-      {:ok, conn} = NIF.wrap(:db_connect, [db])
+      {:ok, db} = NIF.db_open(":memory:")
+      {:ok, conn} = NIF.db_connect(db)
 
       {:ok, 0} =
-        NIF.wrap(:conn_execute, [
+        NIF.conn_execute(
           conn,
           "CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)",
           []
-        ])
+        )
 
       data = [
         ["Alice", "alice@example.com"],
@@ -118,63 +115,49 @@ defmodule Turxo.NIFTest do
 
       for pair <- data do
         {:ok, 1} =
-          NIF.wrap(:conn_execute, [
+          NIF.conn_execute(
             conn,
             "INSERT INTO users (name, email) VALUES (:name, :email)",
             pair
-          ])
+          )
       end
 
       %{db: db, conn: conn, data: data}
     end
 
     test "no parameters", %{conn: conn, data: data} do
-      assert {:ok, [[3]]} = NIF.wrap(:conn_query, [conn, "SELECT COUNT(*) FROM users", []])
-      assert {:ok, ^data} = NIF.wrap(:conn_query, [conn, "SELECT name, email FROM users", []])
+      assert {:ok, [[3]]} = NIF.conn_query(conn, "SELECT COUNT(*) FROM users", [])
+      assert {:ok, ^data} = NIF.conn_query(conn, "SELECT name, email FROM users", [])
     end
 
     test "positional parameters", %{conn: conn} do
       assert {:ok, [[1]]} =
-               NIF.wrap(:conn_query, [conn, "SELECT id FROM users WHERE name = (?1)", ["Alice"]])
+               NIF.conn_query(conn, "SELECT id FROM users WHERE name = (?1)", ["Alice"])
 
       assert {:ok, [[nil]]} =
-               NIF.wrap(:conn_query, [conn, "SELECT email FROM users WHERE name = (?1)", ["Bob"]])
+               NIF.conn_query(conn, "SELECT email FROM users WHERE name = (?1)", ["Bob"])
 
       assert {:ok, [["charlie@example.com"]]} =
-               NIF.wrap(:conn_query, [conn, "SELECT email FROM users WHERE id = (?1)", [3]])
+               NIF.conn_query(conn, "SELECT email FROM users WHERE id = (?1)", [3])
     end
 
     test "named parameters", %{conn: conn} do
       assert {:ok, [[1]]} =
-               NIF.wrap(:conn_query, [
+               NIF.conn_query(
                  conn,
                  "SELECT id FROM users WHERE name = (:name)",
-                 [name: "Alice"]
-               ])
+                 name: "Alice"
+               )
 
       assert {:ok, [[nil]]} =
-               NIF.wrap(:conn_query, [
+               NIF.conn_query(
                  conn,
                  "SELECT email FROM users WHERE name = (:name)",
-                 [name: "Bob"]
-               ])
+                 name: "Bob"
+               )
 
       assert {:ok, [["charlie@example.com"]]} =
-               NIF.wrap(:conn_query, [conn, "SELECT email FROM users WHERE id = (:id)", [id: 3]])
-    end
-  end
-
-  describe "wrap/2 correctly wraps" do
-    test "db_open/1" do
-      assert {:ok, db} = NIF.wrap(:db_open, [":memory:"])
-      assert is_reference(db)
-    end
-
-    test "db_connect/1" do
-      {:ok, db} = NIF.wrap(:db_open, [":memory:"])
-
-      assert {:ok, conn} = NIF.wrap(:db_connect, [db])
-      assert is_reference(conn)
+               NIF.conn_query(conn, "SELECT email FROM users WHERE id = (:id)", id: 3)
     end
   end
 end
