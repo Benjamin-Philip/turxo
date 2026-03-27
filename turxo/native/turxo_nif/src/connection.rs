2246c92112f96401fc6e30156bbec7d12ae92b87
Benjamin Philip <benjamin.philip495@gmail.com>
2026-03-27 19:16:19 +0530

    Modularize Rust code base (#3)
    Restructures the code to be more maintainable and sane.

---
+++ turxo/native/turxo_nif/src/connection.rs
@@ -0,0 +1,92 @@
+use crate::statement::{params_atom_to_key, Params, Value};
+use crate::utils::{runtime, send_result, setup_async_env};
+use rustler::{Env, Reference, Resource, ResourceArc};
+use turso::{Connection, Error as TursoError, Rows};
+
+pub struct ConnectionResource {
+    pub conn: Connection,
+}
+
+#[rustler::resource_impl]
+impl Resource for ConnectionResource {
+    fn destructor(self, _env: Env<'_>) {}
+}
+
+// Connection Execution
+
+#[rustler::nif]
+fn conn_execute<'a>(
+    env: Env<'a>,
+    conn_resource: ResourceArc<ConnectionResource>,
+    sql: String,
+    params: Params,
+) -> Reference<'a> {
+    let (erl_ref, pid, owned_env, owned_ref) = setup_async_env(env);
+
+    runtime().spawn(async move {
+        let result = match params {
+            Params::Positional(p) => conn_resource.conn.execute(sql, p).await,
+            Params::Named(n) => {
+                conn_resource
+                    .conn
+                    .execute(sql, params_atom_to_key(&owned_env, n))
+                    .await
+            }
+        };
+
+        let result = result.map_err(|e| e.to_string());
+        send_result::<u64>(result, pid, owned_env, owned_ref);
+    });
+
+    erl_ref
+}
+
+// Connection Queries
+
+#[rustler::nif]
+fn conn_query<'a>(
+    env: Env<'a>,
+    conn_resource: ResourceArc<ConnectionResource>,
+    sql: String,
+    params: Params,
+) -> Reference<'a> {
+    let (erl_ref, pid, owned_env, owned_ref) = setup_async_env(env);
+
+    runtime().spawn(async move {
+        let rows = match params {
+            Params::Positional(p) => conn_resource.conn.query(sql, p).await,
+            Params::Named(n) => {
+                conn_resource
+                    .conn
+                    .query(sql, params_atom_to_key(&owned_env, n))
+                    .await
+            }
+        };
+
+        let result = match rows {
+            Ok(rows) => decode_rows(rows).await.map_err(|e| e.to_string()),
+            Err(e) => Err(e.to_string()),
+        };
+
+        send_result(result, pid, owned_env, owned_ref);
+    });
+
+    erl_ref
+}
+
+async fn decode_rows(mut rows: Rows) -> Result<Vec<Vec<Value>>, TursoError> {
+    let count = rows.column_count();
+    let mut decoded = Vec::new();
+
+    while let Some(row) = rows.next().await? {
+        let mut decoded_row: Vec<Value> = Vec::new();
+
+        for idx in 0..count {
+            decoded_row.push(Value::new(row.get_value(idx).unwrap()));
+        }
+
+        decoded.push(decoded_row);
+    }
+
+    Ok(decoded)
+}
---
+++ turxo/native/turxo_nif/src/statement.rs
@@ -0,0 +1,64 @@
+use rustler::{Atom, NifUntaggedEnum, OwnedEnv};
+use turso::{Error as TursoError, IntoValue, Value as TursoValue};
+
+#[derive(NifUntaggedEnum, Debug, Clone)]
+pub enum Params {
+    Positional(Vec<Value>),
+    Named(Vec<(Atom, Value)>),
+}
+
+pub fn params_atom_to_key(
+    owned_env: &OwnedEnv,
+    params: Vec<(Atom, Value)>,
+) -> Vec<(String, Value)> {
+    owned_env.run(|env| {
+        params
+            .into_iter()
+            .map(|(key, val)| {
+                (
+                    ":".to_owned() + &key.to_term(env).atom_to_string().unwrap(),
+                    val,
+                )
+            })
+            .collect::<Vec<_>>()
+    })
+}
+
+#[derive(NifUntaggedEnum, Debug, Clone)]
+pub enum Value {
+    Null(Atom),
+    Integer(i64),
+    Real(f64),
+    Text(String),
+    Blob(Vec<u8>),
+}
+
+impl IntoValue for Value {
+    fn into_value(self) -> Result<TursoValue, TursoError> {
+        match self {
+            Value::Null(_i) => Ok(TursoValue::Null),
+            Value::Integer(i) => Ok(TursoValue::Integer(i)),
+            Value::Real(f) => Ok(TursoValue::Real(f)),
+            Value::Text(s) => Ok(TursoValue::Text(s)),
+            Value::Blob(b) => Ok(TursoValue::Blob(b)),
+        }
+    }
+}
+
+impl Value {
+    pub fn new(value: TursoValue) -> Value {
+        match value {
+            TursoValue::Null => Value::Null(rustler::types::atom::nil()),
+            TursoValue::Integer(i) => Value::Integer(i),
+            TursoValue::Real(f) => Value::Real(f),
+            TursoValue::Text(s) => Value::Text(s),
+            TursoValue::Blob(b) => Value::Blob(b),
+        }
+    }
+}
+
+impl From<Value> for TursoValue {
+    fn from(value: Value) -> TursoValue {
+        value.into_value().unwrap()
+    }
+}
